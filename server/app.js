c051b65ccfbbfa5b6c4b15b8c3cdcb3bc896aab3
dwivedi.ece <dwivedi.ece@gmail.com>
2018-03-16 15:56:25 -0400

    initital

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ let app = express();
 
 app.use(express.static(__dirname + "/../client"));
 
-app.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=506f009ebefc48939aa39ae78c47e437',
+app.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=key',
 function(req, res) {
   console.log(res);
   res.json(res.data);
